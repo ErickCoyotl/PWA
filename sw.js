54bf357302b412727eddbc598d948a9a3ab4694c
ErickCoyotl <135668628+ErickCoyotl@users.noreply.github.com>
2023-11-28 12:48:31 -0600

    prueba

--- sw.js
+++ sw.js
@@ -2,6 +2,20 @@ self.addEventListener('install', event => {
     event.waitUntil(
       caches.open('mi-cache').then(cache => {
         return cache.addAll([
+          './',
+          './index.html',
+          './style.css',
+          './datos.html',
+          './pantallas.html',
+          './plataformas.html',
+          './reporte.html',
+          './vistas.html',
+          './script.js',
+          './img/estilos1.png',
+          './img/estilos2.png',
+          './img/Home.png',
+          './img/html.png',
+          './img/script.png',
           './img/splash.png'
         ]);
       })
